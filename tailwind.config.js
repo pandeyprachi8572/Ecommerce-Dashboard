c55170e834bdc5a11ea0af4adba033d8268ad55b
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-11-29 18:06:34 +0530

    *

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [],
 }
 
+// require('@tailwindcss/aspect-ratio') , require('tailwindcss/form')
