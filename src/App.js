f782db38b36567d8c7ea6594796759ef0a23793e
Aradhya <pandeyprachi8572@gmail.com>
2024-12-02 00:00:08 +0530

    *

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
-import ProductList from './features/product-list/ProductList';
-
+import "./App.css";
+//import ProductList from './features/product-list/ProductList';
+import { Counter } from "./features/counter/Counter";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className='App'>
-      <ProductList/>
+    <div className="App">
+      <Home />
     </div>
-  )
+  );
 }
-
 export default App;
