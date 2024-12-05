6e95bc1dca9183ccccd92482d353c0576ada8217
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-05 17:31:31 +0530

    *

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
-import Navbar from "../features/navbar/Navbar";
-import ProductList from "../features/product-list/ProductList";
-//import ProductList from "../features/product-list/ProductList";
+import { Link } from "react-router-dom";
+import NavBar from "../features/navbar/Navbar";
+import ProductList from "../features/product/components/ProductList";
+import Footer from "../features/common/Footer";
+
 function Home() {
-return (
-<div>
-<Navbar>        
-</Navbar>
-<ProductList/>
-</div>
-);
+    return ( 
+        <div>
+            <NavBar>
+                <ProductList></ProductList>
+            </NavBar>
+            <Footer></Footer>
+        </div>
+     );
 }
+
 export default Home;
